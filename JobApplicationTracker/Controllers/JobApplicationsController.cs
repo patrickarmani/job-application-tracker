439e3255feeda9858268a56bd1c7d0f8d949ba6e
Patrick Armani <parmani@byupathway.edu>
2026-09-23 21:19:04 -0300

    feat: retrieve job applications for listing

---
+++ JobApplicationTracker/Controllers/JobApplicationsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using JobApplicationTracker.Data;
+
+namespace JobApplicationTracker.Controllers
+{
+    public class JobApplicationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public JobApplicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public IActionResult Index()
+        {
+            var applications = _context.JobApplications.ToList();
+            return View(applications);
+        }
+
+    }
+
+
+}
