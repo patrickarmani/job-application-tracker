de58f9d15cb5eba93128f5138e084803a017f329
Ovinson Lugo <ovinsonlugo12@gmail.com>
2026-09-25 03:06:24 -0600

    feat: enhance HomeController to retrieve job applications and update Index view layout

--- JobApplicationTracker/Controllers/HomeController.cs
+++ JobApplicationTracker/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using JobApplicationTracker.Models;
+using JobApplicationTracker.Data;
 
 namespace JobApplicationTracker.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly ApplicationDbContext _context;
+    public HomeController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        var applications = _context.JobApplications.ToList();
+        return View(applications);
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
